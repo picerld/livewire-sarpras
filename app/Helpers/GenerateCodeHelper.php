49469800c0205467a3d6172cd4e3d5644ba4ef61
picerld <raficahyadi1221@gmail.com>
2024-08-07 12:01:32 +0700

    fix(helper): code

--- app/Helpers/GenerateCodeHelper.php
+++ app/Helpers/GenerateCodeHelper.php
@@ -2,7 +2,6 @@
 
 namespace App\Helpers;
 
-<<<<<<< HEAD
 use App\Models\Category;
 
 class GenerateCodeHelper
@@ -13,12 +12,5 @@ class GenerateCodeHelper
         $aliases = $category->aliases;
 
         return $aliases . time();
-=======
-class GenerateCodeHelper
-{
-    public static function handleGenerateCode($aliases, $id)
-    {
-        return substr(hash('sha256', $aliases . time()), 0, $id);
->>>>>>> faa95b83bec67b4ce7b381a422654c3e64f2496c
     }
 }
