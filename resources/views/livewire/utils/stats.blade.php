469f78fe818f969e596a70e674f5223eacef5703
picerld <raficahyadi1221@gmail.com>
2024-10-05 17:09:21 +0700

    feat: add transaction chart

--- resources/views/livewire/utils/stats.blade.php
+++ resources/views/livewire/utils/stats.blade.php
@@ -1,2 +1,6 @@
-<x-stat class="border dark:border-base-200 shadow text-black dark:text-slate-300/90 {{ $class }}" :title=$title :value=$value 
-        :icon=$icon color="text-black dark:text-slate-200" />
+<x-stat class="rounded-xl border bg-card text-card-foreground shadow text-black dark:text-slate-300/90 {{ $class }}"
+    :title="$title"
+    :value="$value"
+    :icon="$icon"
+    color="text-black"
+    description="This month" />
